09f82500e2409bc11410e7acf4e3ac06e9ae03de
Frankkiv <lufrankkiv@gmail.com>
2017-03-22 16:43:02 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## 地址選單 DEMO
+
+#### QUICK START
+```xml
+$ git clone https://github.com/frankkiv/AddressSelectDemo.git
+$ cd AddressSelectDemo
+/AddressSelectDemo $ npm install
+/AddressSelectDemo node app.js #server will be host on 127.0.0.1:3000
+```
+<br>
+
+#### HTML SELECT
+- jQuery Mobile
+    - [Demo](http://demos.jquerymobile.com/1.4.5/selectmenu/)
+    - [Guthub](https://github.com/jquery/jquery-mobile)
+
+- Trigger JavaScript ONCHANGE event even when the option is not changed
+    - [StackOverflow](http://stackoverflow.com/questions/11877527/html-select-trigger-javascript-onchange-event-even-when-the-option-is-not-chan)
+
+- Adding options to a dynamically loaded select
+    - [StackOverflow](http://stackoverflow.com/questions/5942613/adding-options-to-a-dynamically-loaded-select)
+
+<br>
+
+#### 台灣 縣市，鄉鎮，地址 中英文 JSON
+- [Github Reference](https://github.com/donma/TaiwanAddressCityAreaRoadChineseEnglishJSON)
+<img src="https://i.imgur.com/rLyCk27.png" style="height:500px">
---
+++ public/parsing.js
@@ -0,0 +1,59 @@
+var jsonObject;
+var cityIndex;
+var areaIndex;
+var addrText;
+//init parsing Joson Object file
+$.getJSON("AllData.json", function(data){
+    jsonObject = data;
+    //for cat address log
+    /*for(var i in jsonObject){
+        console.log(jsonObject[i].CityName);
+        for(var j in data[i].AreaList){
+            console.log("   "+data[i].AreaList[j].AreaName);
+        }
+    }*/
+    $.each(data, function (key, value) {
+        $('#select-city').append(
+            $("<option></option>")
+            .attr("value", key)
+            .text(value.CityName)
+        );
+    });
+});
+
+function onSelectCity(data) {
+    console.log( data.value );
+    this.cityIndex = data.value;
+    updatAreaOptions(data.value);
+}
+
+function onSelectArea(data) {
+    console.log( data.value );
+    this.areaIndex = data.value;
+}
+
+function updatAreaOptions(data){
+    $("#select-area").empty();
+    $('#select-area').append(
+        $("<option></option>")
+        .attr("disabled", 'true')
+        .attr("selected", 'true')
+        .attr("style", 'display:none;')
+        .text("選擇區域")
+    );
+    $.each(this.jsonObject[data].AreaList, function (key, value) {
+        $('#select-area').append(
+            $("<option></option>")
+            .attr("value", key)
+            .text(value.AreaName)
+        );
+    });
+}
+
+function addrSubmit(){
+    this.addrText = $('#textAddr').val();
+    let tmpstring = this.jsonObject[this.cityIndex].CityName+""+this.jsonObject[this.cityIndex].AreaList[this.areaIndex].AreaName+""+this.addrText
+    $('#textOutput').append(
+        $("<h2></h2>").text(tmpstring)
+    );
+}
